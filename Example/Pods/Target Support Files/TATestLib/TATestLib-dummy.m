ba0ea4eba44fd3196d9d565b69d5cee2ead84284
tianao <tianao1992@163.com>
2021-04-23 10:14:15 +0800

    first

---
+++ Example/Pods/Target Support Files/TATestLib/TATestLib-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_TATestLib : NSObject
+@end
+@implementation PodsDummy_TATestLib
+@end
---
+++ TATestLib/Classes/TestTAMe.swift
@@ -0,0 +1,15 @@
+//
+//  TestTAMe.swift
+//  Pods-TATestLib_Example
+//
+//  Created by tianao on 2021/4/23.
+//
+
+import UIKit
+
+class TestTAMe: NSObject {
+
+    public func testClickMe() {
+        
+    }
+}
